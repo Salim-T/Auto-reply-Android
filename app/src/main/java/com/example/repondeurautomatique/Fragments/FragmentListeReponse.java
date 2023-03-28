98da8537d06cafadeb529cd1389b89d6567c9203
Salim <tizi_salim@yahoo.fr>
2023-03-28 19:29:39 +0200

    Fragment liste Reponse, ajout d'une liste de reponses automatiques avec un bouton au bas de la page qui permettra à l'utilisateur d'ajouter une réponse à la liste

--- app/src/main/java/com/example/repondeurautomatique/Fragments/FragmentListeReponse.java
+++ app/src/main/java/com/example/repondeurautomatique/Fragments/FragmentListeReponse.java
@@ -2,65 +2,70 @@ package com.example.repondeurautomatique.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.example.repondeurautomatique.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentListeReponse#newInstance} factory method to
+ * Use the {@link FragmentListeReponse} factory method to
  * create an instance of this fragment.
  */
 public class FragmentListeReponse extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ArrayAdapter<String> mAdapter;
 
-    public FragmentListeReponse() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentListeReponse.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentListeReponse newInstance(String param1, String param2) {
-        FragmentListeReponse fragment = new FragmentListeReponse();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private Button addResponseButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_liste_reponse, container, false);
+
+        View result = inflater.inflate(R.layout.fragment_liste_reponse, container, false);
+
+        addResponseButton = result.findViewById(R.id.addResponseButton);
+
+        ListView listContact = result.findViewById(R.id.listResponse);
+
+        mAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_multiple_choice);
+
+        listContact.setAdapter(mAdapter);
+
+        listContact.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        mAdapter.addAll("ok", "Je vous rappelle plus tard", "Je peux pas j'ai piscine");
+
+        addResponseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Récuper ce que l'utilisateur écrit
+                mAdapter.add("new Element");
+            }
+        });
+
+        return result;
     }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //Sauvegarder les données enregistrées par l'utilisateur
+
+    }
+
 }
